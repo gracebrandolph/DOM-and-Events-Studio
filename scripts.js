50ed49cfdece0b073844a5589be89d4ca2d12c63
Grace Randolph <grace.b.randolph@gmail.com>
2019-10-23 20:29:02 -0500

    finished studio class 14

--- scripts.js
+++ scripts.js
@@ -1,2 +1,41 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    
+    console.log('window loaded');
+
+    let x = 0;
+    
+    document.getElementById("takeoff").onclick = function() {
+        if(confirm("Confirm that the shuttle is ready for takeoff.")){
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
+            document.getElementById("shuttleBackground").style.background = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+        }
+    }
+
+    document.getElementById("landing").onclick = function() {
+        alert("The shuttle is landing. Landing gear engaged.");
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
+        document.getElementById("shuttleBackground").style.background = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = 0; 
+    }
+
+    document.getElementById("missionAbort").onclick = function() {
+        if(confirm("Confirm that you want to abort the mission.")){
+            document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+            document.getElementById("shuttleBackground").style.background = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = 0;
+        }
+    }
+
+    document.getElementById("up").onclick = function() {
+        document.getElementById("rocket").style.marginBottom = "10px";
+        //document.getElementById("spaceShuttleHeight").innerHTML += 10000;
+    }
+
+    // document.getElementById("down").onclick = function() {
+    //     document.getElementById("rocket").style.marginTop = "-10";
+    //     //document.getElementById("spaceShuttleHeight").innerHTML += 10000;
+    // }
+});
